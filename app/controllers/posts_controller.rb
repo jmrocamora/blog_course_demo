63bc584200f29fae599867fdd474baae0510ff13
Jose María Rocamora Pamies <jmrocamora@gmail.com>
2016-01-21 16:13:35 +0100

    Añadido CRUD para Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice:"Que pasa Jose! Tu articulo fue guardado correctamente"
+		else
+			render 'new', notice:"Oh no, Jose! No fue posible guardar el post."
+		end
+	end
+
+	def show
+
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Bien! Tu articulo fue actualizado correctamente."
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+end
