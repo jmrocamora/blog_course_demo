0f273453eaadb35d37f893515938e9ad15e29605
Jose María Rocamora Pamies <jmrocamora@gmail.com>
2016-01-22 17:31:15 +0100

    Añadido editar y eliminar de Proyectos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,23 @@ class ProjectsController < ApplicationController
   def show    
   end
   
+  def edit
+  end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Bien! Tu articulo fue actualizado correctamente."
+		else
+			render 'edit'
+		end
+	end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+  
+    
   private
   
   def find_project
